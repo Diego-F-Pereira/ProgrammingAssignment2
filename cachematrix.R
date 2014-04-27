a2480b3d7b985e592970bc0453ce00dc486763c8
Diego F. Pereira <pereira.diego3@gmail.com>
2014-04-27 02:24:42 -0500

    Solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Function    : makeCacheMatrix
+# Purpose     : Creates a special "matrix" object that can cache its inverse
+# Arguments   : x = A numeric squared matrix
+# Usage       : C <- makeCacheMatrix(matrix(c(1,2,4,2,1,1,3,1,2), nrow=3))
 
-## Write a short comment describing this function
+  makeCacheMatrix <- function( x = numeric() ) {
+    i <- NULL
+    set <- function( y ) {
+      x <<- y
+      i <<- NULL
+    }
+    get <- function() x
+    setsolve <- function( solve ) i <<- solve
+    getsolve <- function() i
+    list( set = set, get = get,
+          setsolve = setsolve,
+          getsolve = getsolve )
+  }
 
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+# Function    : cacheSolve
+# Purpose     : Computes the inverse of a squared matrix cached by the
+#               function makeCacheMatrix()
+# Description : Uses the solve() function for calculating the inverse matrix
+# Arguments   : x = An object created by the makeCacheMatrix() function
+# Usage       : cacheSolve(C)
+  
+  cacheSolve <- function( x, ... ) {
+    i <- x$getsolve()
+    if( !is.null( i ) ) {
+      message( "getting cached data" )
+      return( i )
+    }
+    data <- x$get()
+    i <- solve( data, ... )
+    x$setsolve( i )
+    i
+  }
